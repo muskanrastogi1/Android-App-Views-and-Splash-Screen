b4b3bdc47f71d5444aa05662b9c1ef1071c91e5f
Muskan Rastogi <muskanrastogi1730@gmail.com>
2021-05-17 22:34:20 +0530

    updated

--- app/src/main/java/cardview/muskanrastogi1/com/cardview/SplashScreenActivity.java
+++ app/src/main/java/cardview/muskanrastogi1/com/cardview/SplashScreenActivity.java
@@ -31,7 +31,7 @@ public class SplashScreenActivity extends Activity {
 
                 // Start the next activity
                 Intent mainIntent = new Intent().setClass(
-                        SplashScreenActivity.this, ContactInfo.class);
+                        SplashScreenActivity.this, MainActivity.class);
                 startActivity(mainIntent);
 
                 // Close the activity so the user won't able to go back this
